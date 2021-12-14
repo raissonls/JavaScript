1ae2a6c83d84f2edbcf1cba720c057ec886ed439
Raisson Leal Silva <94128269+raissonls@users.noreply.github.com>
2021-12-14 11:08:46 -0300

    Estudos 14/12/21

---
+++ CursoJS/Exercicios/Modulo-D/Aula07/007.js
@@ -0,0 +1,9 @@
+var vel = 80;
+
+console.log('Dirija utilizando o sinto de segurança')
+
+console.log(`A velocidade do carro é ${vel} km/h, fique atento a velocidade permitida da via.`)
+
+if (vel>=70) {
+    console.log('Você ultrapassou a velocidade permitida, MULTADO!!!')
+}
