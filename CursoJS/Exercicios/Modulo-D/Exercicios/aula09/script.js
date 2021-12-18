6b4b330e887c60d82fa30886fd9329fd4e963823
Raisson Leal Silva <94128269+raissonls@users.noreply.github.com>
2021-12-18 11:10:14 -0300

    atualização

--- CursoJS/Exercicios/Modulo-D/Exercicios/aula09/script.js
+++ CursoJS/Exercicios/Modulo-D/Exercicios/aula09/script.js
@@ -1,1 +1,28 @@
-alert('Olá')
+function Carregar(){
+    var corpo = window.document.body
+    var msg = window.document.querySelector('#msg')
+    var imagem = window.document.querySelector('#imagem')
+    var data = new Date()
+    var hora =data.getHours()
+    var min = data.getMinutes()
+
+    msg.innerHTML = `Agora são ${hora}:${min}`
+
+    if ((hora>=0) & (hora<12)) {
+        //BOM DIA
+        imagem.src = 'imagens/manha-redondo-250px.png'
+        corpo.style.background = 'rgb(138, 145, 247)'
+        
+    } else if((hora>=12)&(hora<18)){
+        //BOA TARDE
+        imagem.src = 'imagens/tarde-redondo-250px.png'
+        corpo.style.background = 'rgba(182, 139, 59, 0.685)'
+
+
+    }else{
+        //BOA NOITE
+        imagem.src = 'imagens/noite-redondo-250px.png'
+        corpo.style.background = 'rgba(42, 13, 116, 0.856)'
+
+    }
+}
