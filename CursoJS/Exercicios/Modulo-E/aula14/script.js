a7ea9042985d8e0fd4bcd8c33bd9e253bd59aa99
Raisson Leal Silva <94128269+raissonls@users.noreply.github.com>
2021-12-27 22:48:54 -0300

    final real oficial 27/12/21

--- CursoJS/Exercicios/Modulo-E/aula14/script.js
+++ CursoJS/Exercicios/Modulo-E/aula14/script.js
@@ -6,27 +6,28 @@ function Contar() {
 
     if ((inicio.length == 0) || (fim.length == 0) || (passo.length == 0)) {
         res.innerHTML = 'Impossivel contar variaveis invalidas'
-    } else if(Number(passo) == 0){
+    } else if (Number(passo) == 0) {
         res.innerHTML = 'Impossivel contar passo zero'
     } else {
         res.innerHTML = `Contando: <br>`;
         let ni = Number(inicio)
         let np = Number(passo)
         let nf = Number(fim)
-        
+
         //MODELO FOR
         if (ni < nf) {
             for (let n = ni; n <= nf; (n += np)) {
                 /*Contagem Crescente */
-                    res.innerHTML += `${n} \u{1F449} `;
+                res.innerHTML += `${n} \u{1F449} `;
             }
         } else {
 
             for (let n = ni; n >= nf; (n -= np)) {
                 /*Contagem Decrescente */
-                    res.innerHTML += `${n} \u{1F449} `;
+                res.innerHTML += `${n} \u{1F449} `;
             }
-            res.innerHTML += ` \u{1F3C1}`;
+
         }
+        res.innerHTML += ` \u{1F3C1}`;
     }
 }
