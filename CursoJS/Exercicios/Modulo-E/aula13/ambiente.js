b2094685d718d752d1d7929126289fa1641ebdf9
Raisson Leal Silva <94128269+raissonls@users.noreply.github.com>
2021-12-27 21:18:03 -0300

    utilização de repetições 27/12/21

--- CursoJS/Exercicios/Modulo-E/aula13/ambiente.js
+++ CursoJS/Exercicios/Modulo-E/aula13/ambiente.js
@@ -1,7 +1,26 @@
 var n = 1;
+var Taboada = 6
 
-while (n<=6){
-    console.log(`A repetição atual é ${n}`)
+
+console.log('Teste com while:')
+
+while (n<=10){
+    console.log(`${Taboada} * ${n} = ${n*Taboada}`)
     n++
+}
+
+n=1;
+console.log('\nTeste com do while:')
+
+do{
+    console.log(`${Taboada} * ${n} = ${n*Taboada}`)
+    n++
+}while(n<=10)
+
+console.log('\nTeste com for:')
+n=1;
+
+for(let n = 1; n<=10 ; n++){
+    console.log(`${Taboada} * ${n} = ${n*Taboada}`)
 }
 
